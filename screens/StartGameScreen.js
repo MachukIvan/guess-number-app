0d41678ff24699783f6c9175cae3d47c1e260942
Ivan Machuk <machuk.ivan@gmail.com>
2020-07-05 14:56:26 +0300

    Added Card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card';
+
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" />
           <Button title="Confirm" />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
